18d35a159702ea137467149096793089aee66f2d
Glen Mailer <glenjamin@gmail.com>
2016-06-02 22:39:21 +0100

    Cleanly shutdown process on SIGINT

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require("http");
+require("http-shutdown").extend();
 var redis = require("redis");
 
 var {addr2url, getenv, initLogging} = require("./details");
@@ -11,18 +12,32 @@ var config = {
 
 var log = initLogging();
 
-var client = redis.createClient(config.redis);
-client.on("ready", function() {
+var redisClient = redis.createClient(config.redis);
+redisClient.on("ready", function() {
   log.info("Connected to redis", {url: config.redis});
 });
 
-var app = createApp(log, client);
+var app = createApp(log, redisClient);
 
 // Create embedded HTTP server
-var server = http.createServer(app);
+var server = http.createServer(app).withShutdown();
 server.listen(config.port, function() {
   var addr = server.address();
   var url = addr2url(addr);
   log.info({url: url, port: addr.port}, "Listening");
 });
 
+var SHUTDOWN_WAIT = 5000;
+process.on("SIGINT", function() {
+  log.warn("Shutting down due to SIGINT");
+
+  redisClient.unref();
+  server.shutdown(function() {
+    log.info("http server closed");
+  });
+
+  setTimeout(function() {
+    log.warn("Did not shutdown cleanly after %dms", SHUTDOWN_WAIT);
+    process.exit(1);
+  }, SHUTDOWN_WAIT).unref();
+});
