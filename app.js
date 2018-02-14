3edd60fa9b7b74ce87182fa725bf1177c312fc81
Glen Mailer <glen@stainlessed.co.uk>
2018-02-14 13:57:39 +0000

    Testing

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ function emailPage(params) {
   return `
 <html>
 <head>
-<title>Email Preview</title>
+<title>Email Preview!!</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
